c8c5eadc7e24ff7fcdaae135d3a7c0f0bbeaac96
ashwinthanaraj <ashwin.t@fyle.in>
2021-12-21 11:09:56 +0530

    Change project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/fyle-qbo-app2'),
+      dir: require('path').join(__dirname, './coverage/fyle-qbo-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
