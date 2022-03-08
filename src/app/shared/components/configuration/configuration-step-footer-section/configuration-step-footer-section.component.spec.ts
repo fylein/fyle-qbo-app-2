be9e2215998c4361db9c2f449d733bc6fab1c843
ashwin1111 <winash1998@gmail.com>
2022-03-08 17:14:49 +0530

    Make configuration step footer section as reusable component

---
+++ src/app/shared/components/configuration/configuration-step-footer-section/configuration-step-footer-section.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConfigurationStepFooterSectionComponent } from './configuration-step-footer-section.component';
+
+describe('ConfigurationStepFooterSectionComponent', () => {
+  let component: ConfigurationStepFooterSectionComponent;
+  let fixture: ComponentFixture<ConfigurationStepFooterSectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ConfigurationStepFooterSectionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ConfigurationStepFooterSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
