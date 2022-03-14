46096f872a723fe31b8fce3f4a1bbff1917ba844
ashwin1111 <winash1998@gmail.com>
2022-03-14 13:26:40 +0530

    Fixed toggle position in import settings, moved unwanted global styles to respective components, remove dead code

--- src/app/shared/components/configuration/configuration-select-field/configuration-select-field.component.ts
+++ src/app/shared/components/configuration/configuration-select-field/configuration-select-field.component.ts
@@ -1,6 +1,5 @@
-import { Component, forwardRef, Input, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, NG_VALUE_ACCESSOR, ControlValueAccessor, NG_VALIDATORS } from '@angular/forms';
-import { noop } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { EmployeeSettingFormOption } from 'src/app/core/models/configuration/employee-setting.model';
 import { ExportSettingFormOption } from 'src/app/core/models/configuration/export-setting.model';
 import { DestinationAttribute } from 'src/app/core/models/db/destination-attribute.model';
@@ -10,22 +9,9 @@ import { HelperService } from 'src/app/core/services/core/helper.service';
 @Component({
   selector: 'app-configuration-select-field',
   templateUrl: './configuration-select-field.component.html',
-  styleUrls: ['./configuration-select-field.component.scss'],
-  // TODO: check if this is needed
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => ConfigurationSelectFieldComponent),
-      multi: true
-    },
-    {
-      provide: NG_VALIDATORS,
-      useExisting: ConfigurationSelectFieldComponent,
-      multi: true,
-    }
-  ]
+  styleUrls: ['./configuration-select-field.component.scss']
 })
-export class ConfigurationSelectFieldComponent implements ControlValueAccessor, OnInit {
+export class ConfigurationSelectFieldComponent implements OnInit {
 
   @Input() form: FormGroup;
   // Having any here is okay, we get a different types of array of objects
@@ -44,22 +30,6 @@ export class ConfigurationSelectFieldComponent implements ControlValueAccessor, 
   constructor(
     public helperService: HelperService
   ) { }
-
-  writeValue(obj: any) {
-    this.value = obj;
-  }
-
-  private onChangeCallback: (_: any) => void = noop;
-
-  private onTouchedCallback: () => void = noop;
-
-  registerOnChange(fn: any) {
-    this.onChangeCallback = fn;
-  }
-
-  registerOnTouched(fn: any) {
-    this.onTouchedCallback = fn;
-  }
 
   ngOnInit(): void {
   }
