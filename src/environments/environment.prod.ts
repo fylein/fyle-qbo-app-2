013ca11725bcc8adc28090a4f8749bc2650c09ce
ashwin1111 <winash1998@gmail.com>
2022-03-30 13:46:49 +0530

    update initial secret values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,17 @@
 export const environment = {
-  production: 'PRODUCTION',
-  fyle_url: 'FYLE_URL',
-  fyle_client_id: 'FYLE_CLIENT_ID',
-  callback_uri: 'CALLBACK_URI',
-  api_url: 'API_URL',
-  app_url: 'ANGULAR APP URL',
-  qbo_client_id: 'QBO_CLIENT_ID',
-  qbo_scope: 'QBO_SCOPE',
-  qbo_authorize_uri: 'QBO_AUTHORIZE_URI',
-  qbo_app_url: 'QBO_APP_URL',
-  hotjar_id : 'HOTJAR_ID',
-  sentry_dsn: 'SENTRY_DSN',
-  release: 'RELEASE',
-  fyle_app_url: 'FYLE_APP_URL'
+  production: '{{PRODUCTION}}',
+  fyle_url: '{{FYLE_URL}}',
+  fyle_client_id: '{{FYLE_CLIENT_ID}}',
+  callback_uri: '{{CALLBACK_URI}}',
+  api_url: '{{API_URL}}',
+  app_url: '{{ANGULAR APP URL}}',
+  qbo_client_id: '{{QBO_CLIENT_ID}}',
+  qbo_scope: '{{QBO_SCOPE}}',
+  qbo_authorize_uri: '{{QBO_AUTHORIZE_URI}}',
+  qbo_app_url: '{{QBO_APP_URL}}',
+  hotjar_id : '{{HOTJAR_ID}}',
+  sentry_dsn: '{{SENTRY_DSN}}',
+  release: '{{RELEASE}}',
+  fyle_app_url: '{{FYLE_APP_URL}}'
 };
 
