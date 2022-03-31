ebe1bc7fa7bca24fc3a3f6a2854a5d684f20e9ae
ashwin1111 <winash1998@gmail.com>
2022-03-31 15:18:37 +0530

    Fix few tests

--- src/app/core/interceptors/jwt.interceptor.spec.ts
+++ src/app/core/interceptors/jwt.interceptor.spec.ts
@@ -1,14 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
+import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { JwtInterceptor } from './jwt.interceptor';
-
 describe('JwtInterceptor', () => {
   beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule,HttpClientModule],
     providers: [
-      JwtInterceptor
+      {
+        provide: JWT_OPTIONS,
+        useValue: JWT_OPTIONS,
+      },
+      JwtInterceptor,
+      JwtHelperService
       ]
   }));
-
   it('should be created', () => {
     const interceptor: JwtInterceptor = TestBed.inject(JwtInterceptor);
     expect(interceptor).toBeTruthy();
