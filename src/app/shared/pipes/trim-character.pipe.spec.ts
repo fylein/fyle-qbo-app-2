233482b4a6b8465fb7dac600cf6ea52daf007ab4
Dhaarani S <dhaaranis@Dhaaranis-MacBook-Air.local>
2022-04-11 13:41:13 +0800

    Pipe testing commit 1

--- src/app/shared/pipes/trim-character.pipe.spec.ts
+++ src/app/shared/pipes/trim-character.pipe.spec.ts
@@ -5,4 +5,10 @@ describe('TrimCharacterPipe', () => {
     const pipe = new TrimCharacterPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('TrimCharacterPipe Transform function check',() => {
+    const name = 'FyleIntegration'
+    const pipe = new TrimCharacterPipe();
+    expect(pipe.transform(name,4)).toEqual('Fyle...')
+  })
 });
