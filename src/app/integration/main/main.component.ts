f692efa7a74a013ea2a23de9ea01275feb564ca8
ashwin1111 <winash1998@gmail.com>
2022-04-18 18:41:05 +0530

    remove logger

--- src/app/integration/main/main.component.ts
+++ src/app/integration/main/main.component.ts
@@ -161,7 +161,6 @@ export class MainComponent implements OnInit {
     });
     this.router.events.subscribe((val) => {
       if (val instanceof NavigationStart) {
-        console.log(val.url)
         this.markModuleActive(val.url);
       }
     });
