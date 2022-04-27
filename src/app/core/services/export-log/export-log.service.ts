6319b81896446a93f7cdd8e3a1141cd3c481b9af
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2022-04-27 12:35:33 +0530

    Fix date filter in Export Logs and dashboard exports effects (#86)

--- src/app/core/services/export-log/export-log.service.ts
+++ src/app/core/services/export-log/export-log.service.ts
@@ -32,8 +32,8 @@ export class ExportLogService {
     if (selectedDateFilter) {
       const startDate = selectedDateFilter.startDate.toLocaleDateString().split('/');
       const endDate = selectedDateFilter.endDate.toLocaleDateString().split('/');
-      params['start_date'] = `${startDate[2]}-${startDate[1]}-${startDate[0]}`;
-      params['end_date'] = `${endDate[2]}-${endDate[1]}-${endDate[0]}`;
+      params['start_date'] = `${startDate[2]}-${startDate[1]}-${startDate[0]}T00:00:00`;
+      params['end_date'] = `${endDate[2]}-${endDate[1]}-${endDate[0]}T23:59:59`;
     }
 
     if (exportedAt) {
