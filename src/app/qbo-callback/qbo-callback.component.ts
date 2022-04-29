7e6dd29a1b092870e22931bb96e598a02a1573ee
ashwin1111 <winash1998@gmail.com>
2022-04-29 13:32:31 +0530

    Handle QBO disconnects for onboarded orgs

--- src/app/qbo-callback/qbo-callback.component.ts
+++ src/app/qbo-callback/qbo-callback.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
+import { environment } from 'environment.localhost';
+import { QboConnectorPost, QBOCredentials } from '../core/models/configuration/qbo-connector.model';
+import { OnboardingState } from '../core/models/enum/enum.model';
+import { QboConnectorService } from '../core/services/configuration/qbo-connector.service';
+import { WorkspaceService } from '../core/services/workspace/workspace.service';
 
 @Component({
   selector: 'app-qbo-callback',
@@ -9,11 +15,31 @@ import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 export class QboCallbackComponent implements OnInit {
 
   constructor(
+    private qboConnectorService: QboConnectorService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private workspaceService: WorkspaceService
   ) { }
 
-  ngOnInit(): void {
+  private postQboCredentials(code: string, realmId: string): void {
+    const qboAuthResponse: QboConnectorPost = {
+      code: code,
+      realm_id: realmId,
+      redirect_uri: `${environment.app_url}/qbo_callback`
+    };
+
+    this.qboConnectorService.connectQBO(qboAuthResponse).subscribe((qboCredentials: QBOCredentials) => {
+      this.router.navigate([`/workspaces/main/dashboard`]);
+    }, (error) => {
+      // TODO: personalise the message based on the error (if it's an actual error / different company connect)
+      const errorMessage = 'message' in error.error ? error.error.message : 'Failed to connect to QuickBooks Online. Please try again';
+      this.snackBar.open(errorMessage, '', { duration: 7000 });
+      this.router.navigate([`/workspaces/onboarding/landing`]);
+    });
+  }
+
+  private checkProgressAndRedirect(): void {
     const navigationExtras: NavigationExtras = {
       queryParams: {
         code: this.route.snapshot.queryParams.code,
@@ -21,6 +47,16 @@ export class QboCallbackComponent implements OnInit {
       }
     };
 
-    this.router.navigate(['workspaces/onboarding/qbo_connector'], navigationExtras);
+    const onboardingState: OnboardingState = this.workspaceService.getOnboardingState();
+
+    if (onboardingState !== OnboardingState.COMPLETE) {
+      this.router.navigate(['workspaces/onboarding/qbo_connector'], navigationExtras);
+    } else {
+      this.postQboCredentials(this.route.snapshot.queryParams.code, this.route.snapshot.queryParams.realmId);
+    }
+  }
+
+  ngOnInit(): void {
+    this.checkProgressAndRedirect();
   }
 }
