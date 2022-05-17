7d50dcee0cdd5dd07da5fd0b6ef7fff483b3b14c
ashwin1111 <winash1998@gmail.com>
2022-05-17 12:46:09 +0530

    add env name

--- config.index.ts
+++ config.index.ts
@@ -6,7 +6,7 @@ console.log('complete', process.env)
 console.log("hello",process.env.API_URL)
 const envConfigFile = `export const environment = {
   production: false,
-  fyle_url: ${process.env.FYLE_URL},
+  fyle_url: '${process.env.FYLE_URL}',
   fyle_client_id: '${process.env.FYLE_CLIENT_ID}',
   callback_uri: '${process.env.CALLBACK_URL}',
   api_url: '${process.env.API_URL}',
