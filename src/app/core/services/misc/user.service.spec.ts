029caae87317d67c1f8f990924addb12289d5fc7
DhaaraniCIT <dhaaranieaswari@gmail.com>
2022-05-17 18:18:26 +0530

    CI variable commit 14

--- src/app/core/services/misc/user.service.spec.ts
+++ src/app/core/services/misc/user.service.spec.ts
@@ -16,23 +16,7 @@ describe('UserService', () => {
   });
   const realuser = localStorage.getItem('user')
 
-  it('getUserdetails service', () => {
-    const response:MinimalUser = {
-      access_token: "fyle",
-      email: "sravan.kumar@fyle.in",
-      full_name: "sravan k",
-      org_id: "orunxXsIajSE",
-      org_name: "Test Sample Statement - GBP",
-      refresh_token: "fyle",
-      user_id: "ust5Ga9HC3qc",
-    };
-    const actualResponse:MinimalUser = service.getUserProfile();
-    actualResponse.access_token="fyle";
-    actualResponse.refresh_token='fyle';
-    expect(actualResponse).toEqual(response || null);
-  })
-
-  it('setUserDetails service', () => {
+  it('setUserDetails getUserdetails service', () => {
     const user:MinimalUser = {
       access_token: "fyle",
       email: "sravan.kumar@fyle.in",
@@ -43,8 +27,10 @@ describe('UserService', () => {
       user_id: "ust5Ga9HC3qc",
     };
     service.storeUserProfile(user);
-    const response = localStorage.getItem('user');
-    expect(response).toBeDefined();
+    const response = service.getUserProfile();
+    const responseKeys = Object.keys(response).sort();
+    const actualKeys = Object.keys(user).sort();
+    expect(responseKeys).toEqual(actualKeys);
   })
 
   it('storeFyleOrgsCount service', (done) => {
