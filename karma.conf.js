11eca8a50112164015d2dbdb3f16a808dd4aa9ae
ashwin1111 <winash1998@gmail.com>
2022-05-18 19:57:20 +0530

    test commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/fyle-qbo-app'),
+      dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [
         { type: 'html' },
