c254e8a36d1de2b6ffbfb83a78d7f7543318975c
ashwin1111 <winash1998@gmail.com>
2022-05-18 19:36:25 +0530

    fix broken test

--- config.index.ts
+++ config.index.ts
@@ -10,6 +10,7 @@ const envConfigFile = `export const environment = {
   api_url: '${process.env.API_URL}',
   app_url: '${process.env.APP_URL}',
   qbo_client_id: '${process.env.QBO_CLIENT_ID}',
+  old_qbo_app_url: '${process.env.OLD_QBO_APP_URL}',
   qbo_scope: '${process.env.QBO_SCOPE}',
   qbo_authorize_uri: '${process.env.QBO_AUTHORIZE_URI}',
   qbo_app_url: '${process.env.QBO_APP_URL}',
