37b5f23b38025de89723fed5ae59ed0d56691f08
Dhaarani <55541808+DhaaraniCIT@users.noreply.github.com>
2022-05-18 16:02:57 +0530

    Testing phase3 testing commit (#99)
    Testing phase3 testing commit

--- src/app/core/services/dashboard/dashboard.service.spec.ts
+++ src/app/core/services/dashboard/dashboard.service.spec.ts
@@ -2,6 +2,9 @@ import { HttpTestingController, HttpClientTestingModule } from '@angular/common/
 import { getTestBed, TestBed } from '@angular/core/testing';
 import { environment } from 'src/environments/environment';
 import { ExportableExpenseGroup } from '../../models/db/expense-group.model';
+import { LastExport } from '../../models/db/last-export.model';
+import { TaskResponse } from '../../models/db/task-log.model';
+import { ExportMode, TaskLogState, TaskLogType } from '../../models/enum/enum.model';
 import { DashboardService } from './dashboard.service';
 
 describe('DashboardService', () => {
@@ -71,28 +74,49 @@ describe('DashboardService', () => {
     req.flush({});
   })
 
-  // it('getLastExport() service check', () => {
-  //   service.getLastExport().subscribe((value) => {
-  //     expect(value).toBeDefined()
-  //   })
-  // })
+  it('getLastExport() service check', () => {
+    const response:LastExport={
+      created_at: new Date("2022-04-27T06:37:30.296865Z"),
+      export_mode: ExportMode.MANUAL,
+      failed_expense_groups_count: 5,
+      id: 154,
+      last_exported_at: new Date("2022-05-10T08:00:38.440819Z"),
+      successful_expense_groups_count: 0,
+      total_expense_groups_count: 5,
+      updated_at: new Date("2022-05-10T08:00:43.857641Z"),
+      workspace: 216
+    }
+    service.getLastExport().subscribe((value) => {
+      const keys = Object.keys(value).sort();
+      const responseKeys = Object.keys(response).sort();
+      expect(keys).toEqual(responseKeys)
+    })
+    const req = httpMock.expectOne({
+      method: 'GET',
+      url: `${API_BASE_URL}/workspaces/${workspace_id}/export_detail/`,
+    });
+    req.flush(response);
+  })
 
-  // it('getExportErrors() service check', () => {
-  //   service.getExportErrors().subscribe((value) => {
-  //     expect(value).toBeDefined()
-  //   })
-  // })
 
-  // it('getExportErrors() service check', () => {
-  //   service.getExportErrors().subscribe((value) => {
-  //     expect(value).toBeDefined()
-  //   })
-  // })
-
-  // it('getTasks() service check', () => {
-  //   service.getTasks().subscribe((value) => {
-  //     expect(value).toBeDefined()
-  //   })
-  // })
+  it('getTasks() service check', () => {
+    const response:TaskResponse = {
+      count: 0,
+      next: null,
+      previous: null,
+      results: []
+    }
+    const taskType: TaskLogType[] = [TaskLogType.FETCHING_EXPENSE, TaskLogType.CREATING_BILL, TaskLogType.CREATING_EXPENSE, TaskLogType.CREATING_CHECK, TaskLogType.CREATING_CREDIT_CARD_PURCHASE, TaskLogType.CREATING_JOURNAL_ENTRY, TaskLogType.CREATING_CREDIT_CARD_CREDIT, TaskLogType.CREATING_DEBIT_CARD_EXPENSE];
+    service.getTasks(500,[TaskLogState.ENQUEUED, TaskLogState.IN_PROGRESS], [], taskType, null).subscribe((value) => {
+      const responseKeys = Object.keys(response).sort();
+      const actualKeys = Object.keys(value).sort();
+      expect(actualKeys).toEqual(responseKeys)
+    })
+    const req = httpMock.expectOne({
+      method: 'GET',
+      url: `${API_BASE_URL}/workspaces/${workspace_id}/tasks/all/?limit=500&offset=0&status=ENQUEUED,IN_PROGRESS&task_type=FETCHING_EXPENSE,CREATING_BILL,CREATING_EXPENSE,CREATING_CHECK,CREATING_CREDIT_CARD_PURCHASE,CREATING_JOURNAL_ENTRY,CREATING_CREDIT_CARD_CREDIT,CREATING_DEBIT_CARD_EXPENSE`,
+    });
+    req.flush(response);
+  })
 
 });
--- src/app/core/services/misc/mapping.service.spec.ts
+++ src/app/core/services/misc/mapping.service.spec.ts
@@ -1,18 +1,237 @@
-import { TestBed } from '@angular/core/testing';
+import { getTestBed, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { MappingService } from './mapping.service';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { environment } from 'environment.localhost';
+import { EmployeeFieldMapping, FyleField, MappingState, QBOField } from '../../models/enum/enum.model';
+import { ExpenseField } from '../../models/misc/expense-field.model';
+import { MappingStats } from '../../models/db/mapping.model';
+import { MappingSettingResponse } from '../../models/db/mapping-setting.model';
 
 describe('MappingService', () => {
   let service: MappingService;
-
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
+  const API_BASE_URL = environment.api_url
+  const workspace_id = environment.tests.workspaceId
+  
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule]
+      imports: [HttpClientModule,HttpClientTestingModule],
+      providers: [MappingService]
     });
-    service = TestBed.inject(MappingService);
+    injector = getTestBed();
+    service = injector.inject(MappingService);
+    httpMock = injector.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('getQBODestinationAttributes() service check', () => {
+    service.getDistinctQBODestinationAttributes([EmployeeFieldMapping.EMPLOYEE, EmployeeFieldMapping.VENDOR]).subscribe(value => {
+      expect(value).toEqual([])
+    })
+    const req = httpMock.expectOne({
+      method: 'GET',
+      url: `${API_BASE_URL}/workspaces/${workspace_id}/qbo/qbo_attributes/?attribute_types=EMPLOYEE,VENDOR`,
+    });
+      req.flush([]);
+
+  });
+
+  it('getDistinctQBODestinationAttributes() service check', () => {
+    service.getDistinctQBODestinationAttributes([EmployeeFieldMapping.EMPLOYEE, EmployeeFieldMapping.VENDOR]).subscribe(value => {
+      expect(value).toEqual([])
+    })
+    const req = httpMock.expectOne({
+      method: 'GET',
+      url: `${API_BASE_URL}/workspaces/${workspace_id}/qbo/qbo_attributes/?attribute_types=EMPLOYEE,VENDOR`,
+    });
+      req.flush([]);
+  });
+
+  it('getFyleExpenseFields() service check',() => {
+    const response:ExpenseField[]=[
+      {
+          "attribute_type": "COST_CENTER",
+          "display_name": "Cost Center"
+      },
+      {
+          "attribute_type": "PROJECT",
+          "display_name": "Project"
+      },
+      {
+          "attribute_type": "REGION",
+          "display_name": "Region"
+      },
+      {
+          "attribute_type": "XERO_REGION",
+          "display_name": "Xero Region"
+      },
+      {
+          "attribute_type": "XERO_ITEM",
+          "display_name": "Xero Item"
+      },
+      {
+          "attribute_type": "XERO_FIELD",
+          "display_name": "Xero Field"
+      }
+  ]
+    service.getFyleExpenseFields().subscribe(value => {
+      const responseKeys = Object.keys(response).sort();
+      const actualKeys = Object.keys(value).sort();
+      expect(actualKeys).toEqual(responseKeys)
+    })
+    const req = httpMock.expectOne({
+      method: 'GET',
+      url: `${API_BASE_URL}/workspaces/${workspace_id}/fyle/expense_fields/`,
+    });
+      req.flush(response);
+  })
+
+  it('getMappings() service check', () => {
+    const response={
+      "count": 125,
+      "next": `${API_BASE_URL}/workspaces/${workspace_id}/mappings/expense_attributes/?all_alphabets=true&destination_type=ACCOUNT&limit=3&mapped=ALL&mapping_source_alphabets=null&offset=6&source_type=CATEGORY`,
+      "previous": `${API_BASE_URL}/workspaces/${workspace_id}/mappings/expense_attributes/?all_alphabets=true&destination_type=ACCOUNT&limit=3&mapped=ALL&mapping_source_alphabets=null&source_type=CATEGORY`,
+      "results": [
+        {
+          "id": 36,
+          "mapping": [],
+          "attribute_type": "CATEGORY",
+          "display_name": "Category",
+          "value": "Advertising",
+          "source_id": 186449,
+          "auto_mapped": false,
+          "auto_created": false,
+          "active": false,
+          "detail": null,
+          "created_at": new Date("2022-04-29T07:14:58.746099Z"),
+          "updated_at": new Date("2022-04-29T07:14:58.746128Z"),
+          "workspace": 1
+      }
+      ]
+  }
+    service.getMappings(MappingState.ALL,true,1,1,[],FyleField.CATEGORY,QBOField.ACCOUNT).subscribe(value => {
+      const responseKeys = Object.keys(response).sort();
+      const actualResponseKeys = Object.keys(value).sort();
+      expect(actualResponseKeys).toEqual(responseKeys)
+    })
+    const req = httpMock.expectOne({
+      method: 'GET',
+      url: `${API_BASE_URL}/workspaces/${workspace_id}/mappings/expense_attributes/?limit=1&offset=1&all_alphabets=true&mapped=ALL&mapping_source_alphabets=null&source_type=CATEGORY&destination_type=ACCOUNT`,
+    });
+      req.flush(response);
+  });
+
+  it('getQBOEmployees() service check', () => {
+    service.getQBOEmployees().subscribe(value => {
+      expect(value).toEqual([])
+    })
+    const req = httpMock.expectOne({
+      method: 'GET',
+      url: `${API_BASE_URL}/workspaces/${workspace_id}/qbo/employees/`,
+    });
+      req.flush([]);
+  });
+
+  it('getQBOVendors() service check', () => {
+    service.getQBOVendors().subscribe(value => {
+      expect(value).toEqual([])
+    })
+    const req = httpMock.expectOne({
+      method: 'GET',
+      url: `${API_BASE_URL}/workspaces/${workspace_id}/qbo/vendors/`,
+    });
+      req.flush([]);
+  });
+
+  it('getMappingSettings() service check', () => {
+    const response:MappingSettingResponse = {
+      count:0,next:'aa',previous:'aa',results:[]}
+    service.getMappingSettings().subscribe(value => {
+      const responseKeys = Object.keys(response).sort();
+      const actualResponseKeys = Object.keys(value).sort();
+      expect(actualResponseKeys).toEqual(responseKeys)
+    })
+    const req = httpMock.expectOne({
+      method: 'GET',
+      url: `${API_BASE_URL}/workspaces/${workspace_id}/mappings/settings/`,
+    });
+      req.flush(response);
+  });
+
+  it('getMappingStats() service check', () => {
+    const response:MappingStats= {
+      all_attributes_count:3,
+      unmapped_attributes_count:3
+    };
+    service.getMappingStats(EmployeeFieldMapping.EMPLOYEE,EmployeeFieldMapping.VENDOR).subscribe((value)=>{
+      const responseKeys = Object.keys(response).sort();
+      const actualKeys = Object.keys(value).sort();
+      expect(actualKeys).toEqual(responseKeys)
+    })
+    const req = httpMock.expectOne({
+      method: 'GET',
+      url: `${API_BASE_URL}/workspaces/${workspace_id}/mappings/stats/?source_type=EMPLOYEE&destination_type=VENDOR`,
+    });
+      req.flush(response);
+  })
+
+  it('getMappings() service check', () => {
+    const response={
+      "count": 3,
+      "next": `${API_BASE_URL}/workspaces/${workspace_id}/mappings/employee_attributes/?all_alphabets=true&destination_type=EMPLOYEE&limit=1&mapped=ALL&mapping_source_alphabets=null&offset=2`,
+      "previous": `${API_BASE_URL}/workspaces/${workspace_id}/mappings/employee_attributes/?all_alphabets=true&destination_type=EMPLOYEE&limit=1&mapped=ALL&mapping_source_alphabets=null`,
+      "results": [
+        {
+          "id": 3,
+          "employeemapping": [],
+          "attribute_type": "EMPLOYEE",
+          "display_name": "Employee",
+          "value": "gokul.kathiresan@fyle.in",
+          "source_id": "oupTTvXHXCuk",
+          "auto_mapped": false,
+          "auto_created": false,
+          "active": null,
+          "detail": {
+              "user_id": "usCPKib1GyYP",
+              "location": null,
+              "full_name": "Gokul",
+              "department": null,
+              "department_id": null,
+              "employee_code": null,
+              "department_code": null
+          },
+          "created_at": new Date("2022-04-29T07:14:57.819103Z"),
+          "updated_at": new Date("2022-04-29T07:14:57.819149Z"),
+          "workspace": workspace_id
+        }
+      ]
+  }
+    service.getEmployeeMappings(MappingState.ALL,true,1,1,[],EmployeeFieldMapping.EMPLOYEE).subscribe(value => {
+      const responseKeys = Object.keys(response).sort();
+      const actualResponseKeys = Object.keys(value).sort();
+      expect(actualResponseKeys).toEqual(responseKeys)
+    })
+    const req = httpMock.expectOne({
+      method: 'GET',
+      url: `${API_BASE_URL}/workspaces/${workspace_id}/mappings/employee_attributes/?limit=1&offset=1&all_alphabets=true&mapped=ALL&mapping_source_alphabets=null&destination_type=EMPLOYEE`,
+    });
+      req.flush(response);
+  });
+  
+  it('getQBOEmployees() service check', () => {
+    service.triggerAutoMapEmployees().subscribe(value => {
+      expect(value).toEqual({})
+    })
+    const req = httpMock.expectOne({
+      method: 'POST',
+      url: `${API_BASE_URL}/workspaces/${workspace_id}/mappings/auto_map_employees/trigger/`,
+    });
+      req.flush({});
+  });
+
 });
