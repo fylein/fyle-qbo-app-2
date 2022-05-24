0dea49fb0342569bc61c23d2c9466df644577deb
DhaaraniCIT <dhaaranieaswari@gmail.com>
2022-05-24 11:42:32 +0530

    QBO testing phase4 commit

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,4 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import {
@@ -21,31 +20,8 @@ getTestBed().initTestEnvironment(
     teardown: { destroyAfterEach: false }
 },
 );
-
 // Then we find all the tests.
 localStorage.setItem('workspaceId', environment.tests.workspaceId);
-const user = environment.tests.user;
-
-fetch(`${API_BASE_URL}/auth/refresh/`, {
-  method: 'POST',
-  body: JSON.stringify({ refresh_token: user.refresh_token }),
-  headers: { 'Content-Type': 'application/json' }
-}).then(result => result.json()).then(jsonformat => {
-  const accessToken: {access_token: string} = {
-    access_token: jsonformat.access_token,
-  };
-  fetch(`${API_BASE_URL}/user/profile/`, {
-    method: 'GET',
-    headers: { 'Authorization': `Bearer ${accessToken.access_token}` }
-  }).then(result => result.json()).then(jsonformat => {
-    const profile:{access_token: string,org_id:string} = {
-      org_id: jsonformat.data.org_id,
-      access_token: accessToken.access_token
-    };
-    localStorage.setItem('user', JSON.stringify(profile));
-  });
-});
 const context = require.context('./', true, /\.guard.spec|\.service.spec|.model.spec|.pipe.spec\.ts$/);
-//const context = require.context('./', true, /\.guard.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
