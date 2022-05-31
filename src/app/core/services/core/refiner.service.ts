6bdc2323ce29d1dce390322e759ae33513c0cbf6
ashwin1111 <winash1998@gmail.com>
2022-05-31 17:49:05 +0530

    ready to ship

--- src/app/core/services/core/refiner.service.ts
+++ src/app/core/services/core/refiner.service.ts
@@ -1,27 +1,37 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { MinimalUser } from '../../models/db/user.model';
 import { RefinerSurveyType } from '../../models/enum/enum.model';
+import { UserService } from '../misc/user.service';
+import { WorkspaceService } from '../workspace/workspace.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RefinerService {
 
-  private readonly onboardingDoneSurveryID: string = '479f4880-da7c-11ec-83f1-2908310c8a20';
+  private readonly onboardingDoneSurveryID: string = environment.refiner_survey.onboarding_done_survery_id;
+  private readonly exportDoneSurveryID: string = environment.refiner_survey.export_done_survery_id;
+  private readonly user: MinimalUser = this.userService.getUserProfile();
+  private readonly workspaceId: string = this.workspaceService.getWorkspaceId();
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private workspaceService: WorkspaceService
+  ) { }
 
   triggerSurvey(actionName: RefinerSurveyType): void {
     (window as any)._refiner('identifyUser', {
-      id: 'ashwin1113',
-      email: 'ashwin4@ashwin2.ashwin2',
-      name: 'Ashwin24',
+      id: this.user.org_id,
+      email: this.user.email,
+      name: this.user.full_name,
       account: {
-        workspace_id: 364234,
-        workspace_name: 'Fyle HQ 3'
+        workspace_id: this.workspaceId,
+        workspace_name: this.user.org_name
       },
       source: 'Fyle Quickbooks Integration',
       action_name: actionName
     });
-    (window as any)._refiner('showForm', this.onboardingDoneSurveryID);
+    (window as any)._refiner('showForm', actionName === RefinerSurveyType.ONBOARDING_DONE ? this.onboardingDoneSurveryID : this.exportDoneSurveryID);
   }
 }
