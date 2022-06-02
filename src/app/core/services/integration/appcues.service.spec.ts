16f8b5fc888511ebdec7db4c3e15529e083bde53
ashwin1111 <winash1998@gmail.com>
2022-06-02 19:04:42 +0530

    linting fix

--- src/app/core/services/integration/appcues.service.spec.ts
+++ src/app/core/services/integration/appcues.service.spec.ts
@@ -1,4 +1,4 @@
-import { getTestBed, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { AppcuesService } from './appcues.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
@@ -7,16 +7,11 @@ describe('AppcuesService', () => {
   let service: AppcuesService;
 
   beforeEach(() => {
-    let injector: TestBed;
-    let httpMock: HttpTestingController;
-
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [AppcuesService]
     });
-    injector = getTestBed();
     service = TestBed.inject(AppcuesService);
-    httpMock = injector.inject(HttpTestingController);
   });
 
   it('should be created', () => {
