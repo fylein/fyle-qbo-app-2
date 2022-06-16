91dfb40d06435ff6c7fd13769bdea613a534371f
Dhaarani <55541808+DhaaraniCIT@users.noreply.github.com>
2022-06-16 15:15:42 +0530

    QBO component testing phase 2 (#131)
    * QBO component testing phase 2

--- src/app/shared/components/core/confirmation-dialog/confirmation-dialog.component.spec.ts
+++ src/app/shared/components/core/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -1,16 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ConfirmationDialog } from 'src/app/core/models/misc/confirmation-dialog.model';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ConfirmationDialogComponent } from './confirmation-dialog.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
-xdescribe('ConfirmationDialogComponent', () => {
+describe('ConfirmationDialogComponent', () => {
+  const model: ConfirmationDialog = {
+    title: 'FYLE',
+    primaryCtaText: 'FYLE',
+    contents: 'Added successfully',
+    hideSecondaryCTA: false
+  };
   let component: ConfirmationDialogComponent;
   let fixture: ComponentFixture<ConfirmationDialogComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ConfirmationDialogComponent ]
+      declarations: [ConfirmationDialogComponent],
+      imports: [
+        MatButtonModule,
+        MatDialogModule,
+        RouterTestingModule,
+        SharedModule
+      ],
+      providers: [
+        {
+          // I was expecting this will pass the desired value
+          provide: MAT_DIALOG_DATA,
+          useValue: model
+        },
+        {
+          // I was expecting this will pass the desired value
+          provide: MatDialogRef,
+          useValue: {}
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +49,17 @@ xdescribe('ConfirmationDialogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should close dialog when close button clicked', fakeAsync(() => {
+    component.ngOnInit();
+    fixture.detectChanges();
+    const popUpHeader = document.getElementsByTagName('h3')[0] as HTMLHeadElement;
+    const popUpContent = document.getElementsByTagName('p')[1] as HTMLParagraphElement;
+    const popUpPrimary = document.getElementsByTagName('h5')[1] as HTMLHeadElement;
+    const popUpCancel = document.getElementsByTagName('h5')[0] as HTMLHeadElement;
+    expect(popUpHeader.innerText).toEqual(model.title);
+    expect(popUpContent.innerText).toEqual(model.contents);
+    expect(popUpPrimary.innerText).toEqual(model.primaryCtaText);
+    expect(popUpCancel.innerText).toEqual('Cancel');
+  }));
 });
--- src/app/shared/components/dashboard/dashboard-qbo-error-dialog/dashboard-qbo-error-dialog.component.spec.ts
+++ src/app/shared/components/dashboard/dashboard-qbo-error-dialog/dashboard-qbo-error-dialog.component.spec.ts
@@ -1,19 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UserService } from 'src/app/core/services/misc/user.service';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { DashboardQboErrorDialogComponent } from './dashboard-qbo-error-dialog.component';
+import { modelData } from './dashboard-qbo-error-dialog.fixture';
 
-xdescribe('DashboardQboErrorDialogComponent', () => {
+describe('DashboardQboErrorDialogComponent', () => {
   let component: DashboardQboErrorDialogComponent;
   let fixture: ComponentFixture<DashboardQboErrorDialogComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DashboardQboErrorDialogComponent ]
+      declarations: [ DashboardQboErrorDialogComponent ],
+      imports: [
+        MatDialogModule,
+        RouterTestingModule,
+        SharedModule, HttpClientModule, HttpClientTestingModule
+      ],
+      providers: [
+        UserService,
+        {
+          // I was expecting this will pass the desired value
+          provide: MAT_DIALOG_DATA,
+          useValue: modelData
+        },
+        {
+          // I was expecting this will pass the desired value
+          provide: MatDialogRef,
+          useValue: {}
+        }
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    localStorage.setItem('user', JSON.stringify({org_id: 'dummy'}));
     fixture = TestBed.createComponent(DashboardQboErrorDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,4 +47,13 @@ xdescribe('DashboardQboErrorDialogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('setup function', fakeAsync(() => {
+    component.ngOnInit();
+    fixture.detectChanges();
+    const popUpHeader = document.getElementsByTagName('h3')[0] as HTMLHeadElement;
+    const popUpSubHeader = document.getElementsByTagName('h5')[0] as HTMLHeadElement;
+    expect(popUpHeader.innerText).toBe(modelData.error_title);
+    expect(popUpSubHeader.innerText).toBe(modelData.error_detail);
+  }));
 });
