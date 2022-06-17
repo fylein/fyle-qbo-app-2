b41b0a5c6c3865c8ea46703ca012b186773448b7
Fyle <fyle@Fyles-MacBook-Air.local>
2022-06-17 17:31:07 +0530

    qbo-components testing commit

--- src/app/shared/components/core/header/header.component.spec.ts
+++ src/app/shared/components/core/header/header.component.spec.ts
@@ -1,32 +1,99 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, getTestBed, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header.component';
 import { TrimCharacterPipe } from '../../../pipes/trim-character.pipe';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { QboConnectorService } from 'src/app/core/services/configuration/qbo-connector.service';
+import { AuthService } from 'src/app/core/services/core/auth.service';
+import { HelperService } from 'src/app/core/services/core/helper.service';
+import { StorageService } from 'src/app/core/services/core/storage.service';
+import { TrackingService } from 'src/app/core/services/integration/tracking.service';
+import { WindowService } from 'src/app/core/services/core/window.service';
+import { UserService } from 'src/app/core/services/misc/user.service';
+import { WorkspaceService } from 'src/app/core/services/workspace/workspace.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
-xdescribe('HeaderComponent', () => {
+describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
-
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
+  let workspace: WorkspaceService;
+  let qboConnectorService: QboConnectorService;
+  const API_BASE_URL = environment.api_url;
+  const workspace_id = environment.tests.workspaceId;
+  let dialogSpy: jasmine.Spy;
+  const dialogRefSpyObj = jasmine.createSpyObj({ afterClosed: of({}), close: null });
+  dialogRefSpyObj.componentInstance = { body: '' };
   beforeEach(async () => {
+    const localStorageDump = {
+      email: 'fyle@fyle.in',
+      access_token: 'kkk',
+      refresh_token: 'uuuu',
+      employee_email: 'fyle@fyle.in',
+      full_name: 'Fyle Integration',
+      user_id: '12',
+      org_id: '12',
+      org_name: 'Fyle Integration'
+    }
+    localStorage.setItem('user', JSON.stringify(localStorageDump));
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientModule],
-      declarations: [ HeaderComponent, TrimCharacterPipe ]
+      imports: [RouterTestingModule, HttpClientModule, SharedModule, MatDialogModule, HttpClientTestingModule],
+      declarations: [HeaderComponent, TrimCharacterPipe],
+      providers: [AuthService, HelperService, StorageService, TrackingService, WindowService, UserService, WorkspaceService, QboConnectorService]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
+    injector = getTestBed();
+    workspace = injector.inject(WorkspaceService);
+    qboConnectorService = injector.inject(QboConnectorService);
+    httpMock = injector.inject(HttpTestingController);
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    dialogSpy = spyOn(TestBed.get(MatDialog), 'open').and.returnValue(dialogRefSpyObj);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('user variable check', () => {
-    expect(component.user).toBeDefined();
+  it('navigate function check', () => {
+    expect(component.navigateBack()).toBeUndefined();
   });
+
+  it('showOrHideProfileDropdown function check', () => {
+    component.isProfileExpanded = true;
+    fixture.detectChanges();
+    component.showOrHideProfileDropdown();
+    fixture.detectChanges();
+    expect(component.isProfileExpanded).toBeFalse()
+
+    component.isProfileExpanded = false;
+    fixture.detectChanges();
+    component.showOrHideProfileDropdown();
+    fixture.detectChanges();
+    expect(component.isProfileExpanded).toBeTrue();
+  });
+
+  it('showOrHideProfileDropdown function check', () => {
+    component.isHelpSectionExpanded = true;
+    fixture.detectChanges();
+    component.showOrHideHelpDropdown();
+    fixture.detectChanges();
+    expect(component.isHelpSectionExpanded).toBeFalse();
+
+    component.isHelpSectionExpanded = false;
+    fixture.detectChanges();
+    component.showOrHideHelpDropdown();
+    fixture.detectChanges();
+    expect(component.isHelpSectionExpanded).toBeTrue();
+  }); 
 });
--- src/app/shared/components/dashboard/dashboard-export-log-dialog/dashboard-export-log-dialog.component.spec.ts
+++ src/app/shared/components/dashboard/dashboard-export-log-dialog/dashboard-export-log-dialog.component.spec.ts
@@ -1,19 +1,46 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, getTestBed, TestBed } from '@angular/core/testing';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { ExportState } from 'src/app/core/models/enum/enum.model';
+import { ExportLogService } from 'src/app/core/services/export-log/export-log.service';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { DashboardExportLogDialogComponent } from './dashboard-export-log-dialog.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
+import { ExpenseGroupResponse } from 'src/app/core/models/db/expense-group.model';
 
-xdescribe('DashboardExportLogDialogComponent', () => {
+describe('DashboardExportLogDialogComponent', () => {
   let component: DashboardExportLogDialogComponent;
   let fixture: ComponentFixture<DashboardExportLogDialogComponent>;
-
+  let service; ExportLogService;
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
+  const API_BASE_URL = environment.api_url;
+  const workspace_id = environment.tests.workspaceId;
   beforeEach(async () => {
+    localStorage.setItem('user', JSON.stringify({org_id: 'dummy'}));
     await TestBed.configureTestingModule({
-      declarations: [ DashboardExportLogDialogComponent ]
+      imports:[MatDialogModule, SharedModule, HttpClientTestingModule],
+      declarations: [ DashboardExportLogDialogComponent ],
+      providers: [{
+        // I was expecting this will pass the desired value
+        provide: MAT_DIALOG_DATA,
+        useValue: {
+          exportState: ExportState, lastExportedAt: new Date()
+        }
+      },
+      {
+        // I was expecting this will pass the desired value
+        provide: MatDialogRef,
+        useValue: {}
+      },ExportLogService]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    injector = getTestBed();
+    service = injector.inject(ExportLogService);
+    httpMock = injector.inject(HttpTestingController);
     fixture = TestBed.createComponent(DashboardExportLogDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,4 +49,19 @@ xdescribe('DashboardExportLogDialogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('setup function', () => {
+    component.ngOnInit();
+    const response: ExpenseGroupResponse= {
+      count: 0,
+      next: 'null',
+      previous: "xxx",
+      results: []
+    };
+    const exportAt = new Date();
+    const req = httpMock.expectNone(
+      req => req.method === 'GET' && req.url.includes(`${API_BASE_URL}/workspaces/${workspace_id}/fyle/expense_group_settings/`)
+    );
+    //req.flush(response);
+  })
 });
