bb5a786022124c7821b51b32185b6b9cb6435926
Fyle <fyle@Fyles-MacBook-Air.local>
2022-06-22 19:01:17 +0530

    qbo-components testing commit

--- src/app/shared/components/mapping/generic-mapping/generic-mapping.component.spec.ts
+++ src/app/shared/components/mapping/generic-mapping/generic-mapping.component.spec.ts
@@ -179,7 +179,7 @@ describe('GenericMappingComponent', () => {
     };
     const req = httpMock.expectOne({
       method: 'GET',
-      url: `${API_BASE_URL}/workspaces/${workspace_id}/mappings/expense_attributes/?limit=50&offset=3&all_alphabets=false&mapped=ALL&mapping_source_alphabets=dh,fy&source_type=${minimaMappingSetting.source_field}&destination_type=${minimaMappingSetting.destination_field}`
+      url: `${API_BASE_URL}/workspaces/${workspace_id}/mappings/expense_attributes/?limit=10&offset=3&all_alphabets=false&mapped=ALL&mapping_source_alphabets=dh,fy&source_type=${minimaMappingSetting.source_field}&destination_type=${minimaMappingSetting.destination_field}`
     });
     req.flush(response);
   });
@@ -242,7 +242,7 @@ describe('GenericMappingComponent', () => {
   };
   const req = httpMock.expectOne({
     method: 'GET',
-    url: `${API_BASE_URL}/workspaces/${workspace_id}/mappings/expense_attributes/?limit=10&offset=0&all_alphabets=true&mapped=ALL&mapping_source_alphabets=null&source_type=PROJECT&destination_type=CUSTOMER`
+    url: `${API_BASE_URL}/workspaces/${workspace_id}/mappings/expense_attributes/?limit=50&offset=0&all_alphabets=true&mapped=ALL&mapping_source_alphabets=null&source_type=PROJECT&destination_type=CUSTOMER`
   });
   req.flush(response);
 });
