7fee01edc0d5c1d61dca7b516b44a11069796e19
Fyle <fyle@Fyles-MacBook-Air.local>
2022-07-07 17:15:32 +0530

    qbo-components testing commit

--- src/app/core/services/integration/appcues.service.spec.ts
+++ src/app/core/services/integration/appcues.service.spec.ts
@@ -2,19 +2,35 @@ import { TestBed } from '@angular/core/testing';
 
 import { AppcuesService } from './appcues.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+import { user } from 'src/app/integration/main/dashboard/dashboard.fixture';
+import { UserService } from '../misc/user.service';
 
 describe('AppcuesService', () => {
   let service: AppcuesService;
 
   beforeEach(() => {
+    const service2 = {
+      getUserProfile: () => of(user)
+    };
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [AppcuesService]
+      providers: [AppcuesService,
+        { provide: UserService, useValue: service2 }
+      ]
     });
     service = TestBed.inject(AppcuesService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('get function check', () => {
+    expect(service.appcues).toBeUndefined();
+  });
+
+  it('initialiseAppcues function check', () => {
+    expect(service.initialiseAppcues()).toBeUndefined();
   });
 });
