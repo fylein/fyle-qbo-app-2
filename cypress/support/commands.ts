8a58cade02846a19b1d8007260817b1afe09cc6f
ashwin1111 <winash1998@gmail.com>
2022-07-19 21:44:05 +0530

    Dummy POC

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -35,3 +35,18 @@
 //     }
 //   }
 // }
+
+declare namespace Cypress {
+  interface Chainable {
+    login(): void;
+  }
+}
+
+Cypress.Commands.add('login', () => {
+  window.localStorage.setItem('user', JSON.stringify({"email":"ashwin.t@fyle.in","access_token":"xyz","refresh_token":"xyz","full_name":"Ashwin","user_id":"usqywo0f3nBY","org_id":"orHVw3ikkCxJ","org_name":"Anagha Org"}))
+})
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // Don't stop the test execution for 4xx errors
+  return false
+})
