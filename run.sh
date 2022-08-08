c3ae86b6f48e6b1eb50f4190fcd31dcafd2c9919
ashwin1111 <winash1998@gmail.com>
2022-08-08 16:08:28 +0530

    Handle builds without secrets

--- run.sh
+++ run.sh
@@ -6,10 +6,26 @@ fi
 for f in /usr/share/nginx/html/*
 do
     echo "Substituting Environment variables and other stuff in $f ...";
-    # Add all environment variables that are read from html files
+    sed -i $SED_EXTRA_ARGS "s?{{API_URL}}?${API_URL}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{APP_URL}}?${APP_URL}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{OLD_QBO_APP_URL}}?${OLD_QBO_APP_URL}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{FYLE_APP_URL}}?${FYLE_APP_URL}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{CALLBACK_URI}}?${CALLBACK_URI}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{FYLE_CLIENT_ID}}?${FYLE_CLIENT_ID}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{FYLE_URL}}?${FYLE_URL}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{PRODUCTION}}?${PRODUCTION}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{QBO_APP_URL}}?${QBO_APP_URL}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{QBO_AUTHORIZE_URI}}?${QBO_AUTHORIZE_URI}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{QBO_CLIENT_ID}}?${QBO_CLIENT_ID}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{QBO_SCOPE}}?${QBO_SCOPE}?g" $f;
     sed -i $SED_EXTRA_ARGS "s?{{SEGMENT_ID}}?${SEGMENT_ID}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{SENTRY_DSN}}?${SENTRY_DSN}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{SENTRY_ENV}}?${SENTRY_ENV}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{RELEASE}}?${RELEASE}?g" $f;
     sed -i $SED_EXTRA_ARGS "s?{{CLARITY_PROJECT_ID}}?${CLARITY_PROJECT_ID}?g" $f;
     sed -i $SED_EXTRA_ARGS "s?{{REFINER_PROJECT_ID}}?${REFINER_PROJECT_ID}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{REFINER_ONBOARDING_DONE_SURVEY_ID}}?${REFINER_ONBOARDING_DONE_SURVEY_ID}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{REFINER_EXPORT_DONE_SURVEY_ID}}?${REFINER_EXPORT_DONE_SURVEY_ID}?g" $f;
 
 done
 
--- scripts/setup_env.js
+++ scripts/setup_env.js
@@ -1,29 +1,29 @@
 const { writeFile } = require("fs");
 
 const environment = {
-  production: `${process.env.PRODUCTION}`,
-  fyle_client_id: `${process.env.FYLE_CLIENT_ID}`,
-  callback_uri: `${process.env.CALLBACK_URI}`,
-  api_url: `${process.env.API_URL}`,
-  app_url: `${process.env.APP_URL}`,
-  qbo_client_id: `${process.env.QBO_CLIENT_ID}`,
-  old_qbo_app_url: `${process.env.OLD_QBO_APP_URL}`,
-  qbo_scope: `${process.env.QBO_SCOPE}`,
-  qbo_authorize_uri: `${process.env.QBO_AUTHORIZE_URI}`,
-  qbo_app_url: `${process.env.QBO_APP_URL}`,
-  sentry_dsn: `${process.env.SENTRY_DSN ? process.env.SENTRY_DSN : ''}`,
-  sentry_env: `${process.env.SENTRY_ENV ? process.env.SENTRY_ENV : ''}`,
-  release: `${process.env.RELEASE ? process.env.RELEASE : ''}`,
-  fyle_app_url: `${process.env.FYLE_APP_URL}`,
+  production: `${process.env.PRODUCTION ? process.env.PRODUCTION : "false"}`,
+  fyle_client_id: `${process.env.FYLE_CLIENT_ID ? process.env.FYLE_CLIENT_ID : '{{FYLE_CLIENT_ID}}'}`,
+  callback_uri: `${process.env.CALLBACK_URI ? process.env.CALLBACK_URI : '{{CALLBACK_URI}}'}`,
+  api_url: `${process.env.API_URL ? process.env.API_URL : '{{API_URL}}'}`,
+  app_url: `${process.env.APP_URL ? process.env.APP_URL : '{{APP_URL}}'}`,
+  qbo_client_id: `${process.env.QBO_CLIENT_ID ? process.env.QBO_CLIENT_ID : '{{QBO_CLIENT_ID}}'}`,
+  old_qbo_app_url: `${process.env.OLD_QBO_APP_URL ? process.env.OLD_QBO_APP_URL : '{{OLD_QBO_APP_URL}}'}`,
+  qbo_scope: `${process.env.QBO_SCOPE ? process.env.QBO_SCOPE : '{{QBO_SCOPE}}'}`,
+  qbo_authorize_uri: `${process.env.QBO_AUTHORIZE_URI ? process.env.QBO_AUTHORIZE_URI : '{{QBO_AUTHORIZE_URI}}'}`,
+  qbo_app_url: `${process.env.QBO_APP_URL ? process.env.QBO_APP_URL : '{{QBO_APP_URL}}'}`,
+  sentry_dsn: `${process.env.SENTRY_DSN ? process.env.SENTRY_DSN : '{{SENTRY_DSN}}'}`,
+  sentry_env: `${process.env.SENTRY_ENV ? process.env.SENTRY_ENV : '{{SENTRY_ENV}}'}`,
+  release: `${process.env.RELEASE ? process.env.RELEASE : '{{RELEASE}}'}`,
+  fyle_app_url: `${process.env.FYLE_APP_URL ? process.env.FYLE_APP_URL : '{{FYLE_APP_URL}}'}`,
   tests: {
     user: {
-      refresh_token: `${process.env.TESTS_USER_REFRESH_TOKEN}`,
+      refresh_token: `${process.env.TESTS_USER_REFRESH_TOKEN ? process.env.TESTS_USER_REFRESH_TOKEN : '{{TESTS_USER_REFRESH_TOKEN}}'}`,
     },
-    workspaceId: `${process.env.TESTS_WORKSPACEID}`
+    workspaceId: `${process.env.TESTS_WORKSPACEID ? process.env.TESTS_WORKSPACEID : '{{TESTS_WORKSPACEID}}'}`,
   },
   refiner_survey: {
-    onboarding_done_survery_id: `${process.env.REFINER_ONBOARDING_DONE_SURVEY_ID}`,
-    export_done_survery_id: `${process.env.REFINER_EXPORT_DONE_SURVEY_ID}`
+    onboarding_done_survery_id: `${process.env.REFINER_ONBOARDING_DONE_SURVEY_ID ? process.env.REFINER_ONBOARDING_DONE_SURVEY_ID : '{{REFINER_ONBOARDING_DONE_SURVEY_ID}}'}`,
+    export_done_survery_id: `${process.env.REFINER_EXPORT_DONE_SURVEY_ID ? process.env.REFINER_EXPORT_DONE_SURVEY_ID : '{{REFINER_EXPORT_DONE_SURVEY_ID}}'}`,
   }
 };
 
