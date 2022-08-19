e1d0cd55c675b1ea95d1c19c063ad7ba5ad7a630
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2022-08-19 14:46:34 +0530

    e2e test for resolving mapping error (#177)
    * e2e test for resolving mapping error
    
    * Handle Exports Polling smartly
    
    * Update css class
    
    * Remove bizzare assert
    
    * Update file names to run tests in sequence
    
    * Run tests on macos
    
    * Remove container

---
+++ cypress/e2e/journeys/003-resolve-mapping-error.cy.js
@@ -0,0 +1,89 @@
+/// <reference types="cypress" />
+
+describe('resolve mapping error journey', () => {
+  beforeEach(() => {
+    cy.login()
+    cy.visit('/')
+  })
+
+  function importExpenses() {
+    // Wait for sync import from Fyle to be completed
+    cy.wait('@synchronousImport').its('response.statusCode').should('equal', 200)
+    cy.wait('@exportableExpenseGroups').its('response.statusCode').should('equal', 200)
+
+    // Check if exports are ready to be processed
+    cy.get('.export--info-text').contains('Click on Export to start exporting expenses from Fyle as QBO transactions.')
+    cy.get('.zero-state-with-illustration--zero-state-img').should('be.visible')
+  }
+
+  function setupExportsPolling() {
+    // Wait till the exports are processed
+    cy.wait('@tasksPolling').then((http) => {
+      const filteredTasks = http.response.body.results.filter(task => (task.status === 'IN_PROGRESS' || task.status === 'ENQUEUED')).length;
+
+      if (filteredTasks > 0) {
+        setupExportsPolling()
+      } else {
+        assert.equal(filteredTasks, 0, 'All tasks are processed')
+      }
+    })
+  }
+
+  function exportExpenses() {
+    cy.submitButton('Export').click()
+
+    // Check if the export is in progress
+    cy.get('.configuration--submit-btn').should('have.class', 'btn-disabled').contains('Exporting')
+    cy.get('.export--info-text').contains('This may take a few minutes. Please wait...')
+    cy.get('.mat-progress-bar-buffer').as('exportProgressBar').should('be.visible')
+
+    cy.wait('@exportsTrigger').its('response.statusCode').should('equal', 200)
+    cy.wait('@tasksPolling').its('response.statusCode').should('equal', 200)
+
+    setupExportsPolling()
+  }
+
+  function resolveMappingError() {
+    // Check if past export details and errors are visible
+    cy.get('.past-export--content-section').should('be.visible')
+    cy.get('.errors--section').should('be.visible')
+    cy.get('.errors--mapping-error-contents').contains('Employee Mapping errors')
+
+    // Resolve employee mapping error
+    cy.get('.errors--mapping-error-contents').contains('Resolve').click()
+
+    cy.wait('@getDestinationAttributes').its('response.statusCode').should('equal', 200)
+
+    cy.get('.dashboard-resolve-mapping-dialog--header-section').should('be.visible')
+    cy.get('.dashboard-resolve-mapping-dialog--heading').should('be.visible')
+    cy.get('.mat-column-qbo').eq(1).contains('Select Vendor').click()
+    cy.selectMatOption('Amazon')
+
+    cy.get('.dashboard-resolve-mapping-dialog--close-icon').click()
+  }
+
+  function reExportExpenses() {
+    cy.get('.errors--resolved-stat').should('be.visible').contains('error(s) resolved')
+
+    cy.submitButton('Export').click()
+
+    cy.wait('@tasksPolling').its('response.statusCode').should('equal', 200)
+    setupExportsPolling()
+
+    // Integration Errors should not be visible since it is resolved
+    cy.get('.errors--integration-error-contents').should('not.contain', 'Integration Errors')
+  }
+
+  it('should resolve mapping error', () => {
+    // User should be taken to dashboard since they are already onboarded and logged in
+    cy.url().should('include', '/workspaces/main/dashboard')
+
+    importExpenses()
+
+    exportExpenses()
+
+    resolveMappingError()
+
+    reExportExpenses()
+  })
+})
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -11,8 +11,16 @@ declare global {
       saveSetting(content: string): void;
       getMatToggle(toggleIndex: number): void;
       ignoreTokenHealth(): void;
+      setupHttpListeners(): void;
     }
   }
+}
+
+function setupInterceptor(method: 'GET' | 'POST', url: string, alias: string) {
+  cy.intercept({
+    method: method,
+    url: `**${url}**`,
+  }).as(alias);
 }
 
 Cypress.Commands.add('login', () => {
@@ -27,7 +35,25 @@ Cypress.Commands.add('login', () => {
   };
   window.localStorage.setItem('user', JSON.stringify(user))
   window.localStorage.setItem('workspaceId', environment.e2e_tests.workspace_id)
+
+  // cy.login() will be used in all tests, hence adding http listener here
+  cy.setupHttpListeners();
 })
+
+Cypress.Commands.add('setupHttpListeners', () => {
+  // This helps cypress to wait for the http requests to complete with 200, regardless of the defaultCommandTimeout (10s)
+  // Usage: cy.wait('@getDestinationAttributes').its('response.statusCode').should('equal', 200)
+
+  setupInterceptor('GET', '/qbo/destination_attributes/', 'getDestinationAttributes');
+
+  setupInterceptor('POST', '/fyle/expense_groups/sync/', 'synchronousImport');
+
+  setupInterceptor('GET', '/fyle/exportable_expense_groups/', 'exportableExpenseGroups');
+
+  setupInterceptor('GET', '/tasks/all/', 'tasksPolling');
+
+  setupInterceptor('POST', '/exports/trigger/', 'exportsTrigger');
+});
 
 Cypress.Commands.add('selectMatOption', (optionName) => {
   cy.get('mat-option').contains(optionName).click()
