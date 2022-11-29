565d7d647ec6511254474a01018905dbf94a13d8
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2022-11-29 15:36:51 +0530

    Fix broken test - Project Mapping Update (#229)
    * Fix broken test - Project Mapping Update
    
    * Run e2e when there is a change in tests

--- cypress/e2e/002-journeys/002-update-configuration.cy.js
+++ cypress/e2e/002-journeys/002-update-configuration.cy.js
@@ -312,7 +312,6 @@ describe('update configuration', () => {
     cy.url().should('include', '/workspaces/main/mapping/project')
     cy.wait('@getMappingOptions').its('response.statusCode').should('equal', 200)
 
-    cy.get('.mapping-filter--filter-alphabet-list').contains('F').click()
     cy.get('.mapping-table--row').eq(1).as('projectMappingRow')
     cy.get('@projectMappingRow').find('.mat-column-fyle').contains('Aaron Abbott')
 
