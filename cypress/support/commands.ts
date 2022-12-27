369922286b866acc1c2be94bb99e2f47e677cc8a
codefactor-io <support@codefactor.io>
2022-12-27 11:13:30 +0000

    [CodeFactor] Apply fixes

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,4 @@
-/// <reference types="cypress" />
+// / <reference types="cypress" />
 
 import environment from '../../src/environments/environment.json';
 
@@ -42,17 +42,17 @@ Cypress.Commands.add('login', () => {
     org_id: environment.e2e_tests.org_id,
     org_name: 'XYZ Org'
   };
-  window.localStorage.setItem('user', JSON.stringify(user))
-  window.localStorage.setItem('workspaceId', environment.e2e_tests.workspace_id)
+  window.localStorage.setItem('user', JSON.stringify(user));
+  window.localStorage.setItem('workspaceId', environment.e2e_tests.workspace_id);
 
-  // cy.login() will be used in all tests, hence adding http listener here
+  // Cy.login() will be used in all tests, hence adding http listener here
   cy.setupHttpListeners();
-})
+});
 
 Cypress.Commands.add('setupHttpListeners', () => {
   // This helps cypress to wait for the http requests to complete with 200, regardless of the defaultCommandTimeout (10s)
   // Usage: cy.wait('@getDestinationAttributes').its('response.statusCode').should('equal', 200)
-  cy.intercept('POST', '**/refresh_dimensions', {}).as('refreshDimension')
+  cy.intercept('POST', '**/refresh_dimensions', {}).as('refreshDimension');
 
   setupInterceptor('GET', '/qbo/destination_attributes/', 'getDestinationAttributes');
 
@@ -70,46 +70,46 @@ Cypress.Commands.add('setupHttpListeners', () => {
 
   setupInterceptor('GET', '/export_detail', 'getPastExport');
 
-  setupInterceptor('GET', '/fyle/expense_groups/', 'getExpenseGroups')
+  setupInterceptor('GET', '/fyle/expense_groups/', 'getExpenseGroups');
 
-  setupInterceptor('GET', '/mappings/settings', 'getMappingSettings')
+  setupInterceptor('GET', '/mappings/settings', 'getMappingSettings');
 
-  setupInterceptor('GET', '/mappings/expense_attributes/', 'getMappings')
+  setupInterceptor('GET', '/mappings/expense_attributes/', 'getMappings');
 
-  setupInterceptor('GET', '/fyle/expense_fields', 'getFyleExpenseFields')
+  setupInterceptor('GET', '/fyle/expense_fields', 'getFyleExpenseFields');
 
-  setupInterceptor('GET', '/mappings/employee_attributes/', 'getEmployeeMappings')
+  setupInterceptor('GET', '/mappings/employee_attributes/', 'getEmployeeMappings');
 
-  setupInterceptor('GET', '/qbo/vendors/', 'getQBOVendors')
+  setupInterceptor('GET', '/qbo/vendors/', 'getQBOVendors');
 
-  setupInterceptor('GET', '/qbo/mapping_options/', 'getMappingOptions')
+  setupInterceptor('GET', '/qbo/mapping_options/', 'getMappingOptions');
 });
 
 Cypress.Commands.add('selectMatOption', (optionName) => {
-  cy.get('mat-option').contains(optionName).click()
-})
+  cy.get('mat-option').contains(optionName).click();
+});
 
 Cypress.Commands.add('submitButton', (content: string | void) => {
-  const button = cy.get('.configuration--submit-btn')
-  return content ? button.contains(content) : button
-})
+  const button = cy.get('.configuration--submit-btn');
+  return content ? button.contains(content) : button;
+});
 
 Cypress.Commands.add('saveSetting', (content: string) => {
-  cy.submitButton(content).click()
-})
+  cy.submitButton(content).click();
+});
 
 Cypress.Commands.add('getMatToggle', (toggleIndex: number) => {
-  return cy.get('.mat-slide-toggle-bar').eq(toggleIndex)
-})
+  return cy.get('.mat-slide-toggle-bar').eq(toggleIndex);
+});
 
 Cypress.Commands.add('ignoreTokenHealth', () => {
   // Intercept this API call to save some time
-  cy.intercept('GET', '**/qbo/preferences/', {})
-})
+  cy.intercept('GET', '**/qbo/preferences/', {});
+});
 
 Cypress.Commands.add('navigateToSettingPage', (pageName: string) => {
   cy.get('.side-nav-bar--module-block-content').contains(pageName).click();
-})
+});
 
 Cypress.Commands.add('exportsPolling', () => {
   // Wait till the exports are processed
@@ -117,45 +117,45 @@ Cypress.Commands.add('exportsPolling', () => {
     const filteredTasks = http?.response?.body.results.filter((task: any) => (task.status === 'IN_PROGRESS' || task.status === 'ENQUEUED')).length;
 
     if (filteredTasks > 0) {
-      cy.exportsPolling()
+      cy.exportsPolling();
     } else {
-      assert.equal(filteredTasks, 0, 'All tasks are processed')
+      assert.equal(filteredTasks, 0, 'All tasks are processed');
     }
-  })
-})
+  });
+});
 
 Cypress.Commands.add('waitForDashboardLoad', () => {
-  cy.wait('@synchronousImport').its('response.statusCode').should('equal', 200)
-  cy.wait('@exportableExpenseGroups').its('response.statusCode').should('equal', 200)  
-})
+  cy.wait('@synchronousImport').its('response.statusCode').should('equal', 200);
+  cy.wait('@exportableExpenseGroups').its('response.statusCode').should('equal', 200);
+});
 
 Cypress.Commands.add('navigateToModule', (pageName: string) => {
-  cy.get('.side-nav-bar--module-block-text').contains(pageName).click()
-})
+  cy.get('.side-nav-bar--module-block-text').contains(pageName).click();
+});
 
 Cypress.Commands.add('navigateToMappingPage', (pageName: string) => {
   cy.get('.side-nav-bar--module-block-text-inner').contains(pageName).click();
-})
+});
 
 Cypress.Commands.add('importToFyle', (fieldOrder: number, enable: boolean, optionName: string = '') => {
   cy.get('.import-settings--field-toggle-section').eq(fieldOrder).within(() => {
-    cy.enableConfigurationToggle(0)
+    cy.enableConfigurationToggle(0);
     if (enable) {
-      cy.get('.import-settings--fyle-field').click()
+      cy.get('.import-settings--fyle-field').click();
     }
-  })
+  });
   if (enable) {
-    cy.selectMatOption(optionName)
+    cy.selectMatOption(optionName);
   }
-})
+});
 
 Cypress.Commands.add('enableConfigurationToggle', (fieldOrder: number) => {
-  cy.getMatToggle(fieldOrder).click()
-})
+  cy.getMatToggle(fieldOrder).click();
+});
 
 Cypress.Commands.add('selectConfigurationField', (fieldOrder: number, optionName: string) => {
   cy.get('.configuration--field-section').eq(fieldOrder).within(() => {
-    cy.get('.configuration--form-field').first().click()
-  })
-  cy.selectMatOption(optionName)
-})
+    cy.get('.configuration--form-field').first().click();
+  });
+  cy.selectMatOption(optionName);
+});
