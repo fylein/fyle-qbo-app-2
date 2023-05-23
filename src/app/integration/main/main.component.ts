89a03c2b1549444ded7fdf996c29736cca4b8dac
Fyle <fyle@Fyles-MacBook-Air.local>
2023-05-23 11:59:54 +0530

    bug fix commit

--- src/app/integration/main/main.component.ts
+++ src/app/integration/main/main.component.ts
@@ -124,6 +124,10 @@ export class MainComponent implements OnInit {
     if (module.name === 'Mappings' || module.name === 'Configuration') {
       module.isExpanded = !module.isExpanded;
     } else {
+      const route = module.route.split("/", 3)
+      if(route.length === 3) {
+        module.route = route[0]+'/'+route[1]+'_'+route[2];
+      }
       this.router.navigate([`/workspaces/main/${module.route}`]);
     }
   }
