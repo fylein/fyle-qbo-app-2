def5a65f3f2f6aea3d45997710f52493e11034a5
Fyle <fyle@Fyles-MacBook-Air.local>
2023-06-09 12:45:29 +0530

    onboarding update category wait update

--- cypress/e2e/journeys/onboarding.cy.js
+++ cypress/e2e/journeys/onboarding.cy.js
@@ -283,9 +283,10 @@ describe('onboarding journey', () => {
     cy.get('@categoryMappingRow').find('.mat-column-fyle').contains('Food')
 
     cy.get('@categoryMappingRow').find('.mapping-table--form-field').click()
-    cy.get('.search-select--search-input').eq(1).type('Advertising')
+    cy.get('.search-select--search-input').eq(1).type('Food')
     cy.wait('@getMappingOptions').its('response.statusCode').should('equal', 200)
-    cy.get('.mat-option').contains('Advertising').click()
+    cy.get('.mat-option').contains('Food').click()
+    cy.wait(1500)
     cy.navigateToModule('Dashboard')
     // Export
     cy.url().should('include', '/workspaces/main/dashboard')
