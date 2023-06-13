32bde53998666a3710fe5befcc139c099146e926
Fyle <fyle@Fyles-MacBook-Air.local>
2023-06-13 17:53:46 +0530

    Category mapping update

--- cypress/e2e/journeys/onboarding.cy.js
+++ cypress/e2e/journeys/onboarding.cy.js
@@ -283,7 +283,7 @@ describe('onboarding journey', () => {
     cy.get('@categoryMappingRow').find('.mat-column-fyle').contains('Food')
 
     cy.get('@categoryMappingRow').find('.mapping-table--form-field').click()
-    cy.get('.search-select--search-input').eq(1).type(' ')
+    cy.get('.search-select--search-input').eq(1).type('b ')
     cy.wait('@getMappingOptions').its('response.statusCode').should('equal', 200)
     cy.wait(1500)
     cy.get('.mat-option').eq(1).click()
