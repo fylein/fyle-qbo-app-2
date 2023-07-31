c2d35d42eb1dca87b2284f2a14f79f3ec70cfef0
Nilesh Pant <nileshpant6969@gmail.com>
2023-07-31 16:40:20 +0530

    f

--- src/app/core/services/configuration/clone-setting.service.spec.ts
+++ src/app/core/services/configuration/clone-setting.service.spec.ts
@@ -4,13 +4,14 @@ import { CloneSettingService } from './clone-setting.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { mockCloneSettingsGet } from 'src/app/integration/onboarding/clone-settings/clone-settings.fixture';
 import { environment } from 'src/environments/environment';
+import { WorkspaceService } from '../workspace/workspace.service';
 
 describe('CloneSettingService', () => {
   let service: CloneSettingService;
   let injector: TestBed;
   let httpMock: HttpTestingController;
   const API_BASE_URL = environment.api_url;
-  const workspace_id = 1;
+  const workspace_id = environment.tests.workspaceId;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
