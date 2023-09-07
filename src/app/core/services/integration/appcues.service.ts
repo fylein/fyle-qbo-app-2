6959a44532bd671d3d20912d89f4f7bb6ce8fbdc
ruuushhh <rushikeshtayade2001@gmail.com>
2023-09-07 15:42:14 +0530

    Appcues added to QBO

---
+++ src/app/core/services/integration/appcues.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { UserService } from '../misc/user.service';
+import { WorkspaceService } from '../workspace/workspace.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppcuesService {
+
+  constructor(
+    private userService: UserService,
+    private workspaceService: WorkspaceService
+  ) { }
+
+  get appcues() {
+    return (window as any).Appcues;
+  }
+
+  initialiseAppcues(): void {
+    if (this.appcues) {
+      const user = this.userService.getUserProfile();
+      this.appcues.identify(user.user_id, {
+        email: user.email,
+        name: user.full_name,
+        'Org ID': user.org_id,
+        'Workspace ID': this.workspaceService.getWorkspaceId(),
+        'Workspace Name': user.org_name,
+        source: 'Fyle Quickbooks Integration - 2'
+      });
+    }
+  }
+}
